4921542bffc6af2d825c5b4d665b93a8bd058fcb
wisnubdd <wisnu@bolehdicoba.com>
2024-04-17 17:09:13 +0700

    initialized route for getting top blogs

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ app = FastAPI()
 def read_root():
   return {"message": "connected"}
 
+@app.get("/blogs")
+async def get_top_blogs():
+  ids = [1,2,3,4,5]
+  blogs = []
+  for id in ids:
+    blogs.append({
+      "id": id,
+      "title": f"Article {id}",
+      "meta": f"medatada of article {id}",
+      "body": f"Body of article {id}",
+    })
+  return blogs
+
 @app.get("/blog/{id}")
 async def get_blog_by_id(id: int):
   return {
